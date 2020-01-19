2185b5b68f8f63407b789f793dd6e99de7ae6e31
bikilone <milosbirovljevic@gmail.com>
2020-01-19 14:32:07 +0100

    Basic structure

---
+++ script.ts
@@ -0,0 +1,55 @@
+// string
+
+// number
+
+// boolean
+
+// implicit or explicit
+
+// array
+
+// multiple types
+
+// any
+
+// functions
+
+// functions as types
+
+// objects
+
+// objects implicit 
+
+// type alias
+
+// never
+
+// null and strict null checks
+
+// compailer errors, no emmit on erros, source maps, noImplicitAny
+
+// smart compailer, strict null checks
+
+// we dont need babel
+
+// class
+
+// private, public, protected, readonly
+
+// constructor, shortcut for properties
+
+// private methods
+
+// inheritance, extends, overwrite, super()
+
+// getters, setters
+
+// static properties, methods (helpers)
+
+// abstract classes
+
+// interface, optional args, unknown props, methods
+
+// Generics
+
+
